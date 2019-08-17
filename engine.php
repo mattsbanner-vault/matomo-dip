0c2f9738a648d20f93e8252b267cdbe3d64624ad
Matt <mattsbanner@gmail.com>
2019-08-17 15:38:52 +0100

    Add error messages

--- engine.php
+++ engine.php
@@ -11,7 +11,18 @@
 
     $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
     $sql = "UPDATE `option` SET `option_value` = '".$ip_list."' WHERE `option_name` = 'SitesManager_ExcludedIpsGlobal'";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
     echo $sql;
 
     $conn->close();
