7427d1529457111e1419814b83743413ffd0a142
Matt <mattsbanner@gmail.com>
2019-08-17 15:48:17 +0100

    Remove dubugging

--- engine.php
+++ engine.php
@@ -7,24 +7,12 @@
         array_push($ip_addresses, gethostbyname($hostname));
     }
 
-    $ip_list = substr(implode(', ', $ip_addresses),2);
+    $ip_list = substr(implode(',', $ip_addresses),1);
 
     $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
     $sql = "UPDATE `option` SET `option_value` = '".$ip_list."' WHERE `option_name` = 'SitesManager_ExcludedIpsGlobal'";
     
-    if ($conn->query($sql) === TRUE) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error updating record: " . $conn->error;
-    }
-
-    echo $sql;
-
     $conn->close();
 
 ?>
